aae8a7aaddef4d817ddcffda5baa861019e7f390
Ammar Khan <aymar1400@gmail.com>
2025-02-01 19:43:44 -0700

    navbar implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
+import React from "react"
+
+import Home from "./pages/Home.jsx"
+import Merch from "./pages/Merch.jsx"
+import Contact from "./pages/Contact.jsx"
+
 import { Navbar } from "./components/Navbar.jsx";
 
+
 function App() {
+
 
   return (
     <>
       <Navbar />
+
+      <Home />
+      <br />
+      <br />
+
     </>
   )
 }
---
+++ client/src/pages/Merch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Merch() {
+  return(
+    <>
+      <h1>Merch</h1>
+    </>
+  )
+}
+
+export default Merch
