26e83edc245acd8b5b0a8dce1e0aa9270a2f73c3
Ammar Khan <aymar1400@gmail.com>
2025-02-04 20:55:50 -0700

    router implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/Home.jsx"
-import Merch from "./pages/Merch.jsx"
-import Contact from "./pages/Contact.jsx"
+import Home from "./pages/Home.jsx";
+import Merch from "./pages/Merch.jsx";
+import Contact from "./pages/Contact.jsx";
 
 import { Navbar } from "./components/Navbar.jsx";
 
@@ -11,14 +11,14 @@ function App() {
 
 
   return (
-    <>
+    <Router>
       <Navbar />
-
-      <Home />
-      <br />
-      <br />
-
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/merch" element={<Merch />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 
