fe9851759646ba1db821028532511cd76d3af6b9
Ammar Khan <aymar1400@gmail.com>
2025-02-10 21:15:35 -0700

    implemented parallax hero section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,29 +5,45 @@ import maitland_logo from "../assets/shed_transparent.png"
 const Navbar = () => {
   return (
     <>
+      {/*
+      <nav class="bg-black p-4 fixed w-full z-50 top-0 shadow-lg">
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="text-white text-2xl font-bold">
+                GeeksforGeeks
+            </div>
+            <ul class="flex space-x-6 text-white">
+                <li><a href="#home" class="hover:text-yellow-300">Home</a></li>
+                <li><a href="#courses" class="hover:text-yellow-300">Courses</a></li>
+                <li><a href="#articles" class="hover:text-yellow-300">Articles</a></li>
+                <li><a href="#about" class="hover:text-yellow-300">About Us</a></li>
+                <li><a href="#contact" class="hover:text-yellow-300">Contact</a></li>
+            </ul>
+        </div>
+      </nav>
+      */}
 
-      <nav class="hidden border-t md:block bg-white border-black-200 border-b">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={maitland_logo} class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">Blake's Gelato</span>
+      <nav className="hidden border-t md:block bg-white border-black-200 fixed w-full z-50 top-0 shadow-lg">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logo} className="h-12" />
+            <span className="self-center text-3xl font-semibold whitespace-nowrap text-black">Blake's Gelato</span>
           </a>
-          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
           </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:border-gray-700">
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="text-xl flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:border-gray-700">
               <li>
-                <a href="#" class="block py-2 px-3 text-black bg-white rounded-sm md:bg-transparent md:p-0 text-black" aria-current="page">Home</a>
+                <a href="#" className="block py-2 px-3 text-black bg-white rounded-sm md:bg-transparent md:p-0 text-black" aria-current="page">Home</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark: md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">Merch</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark: md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">Merch</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark: md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">Contact Us</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark: md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">Contact Us</a>
               </li>
               <li>
               </li>
@@ -50,6 +66,7 @@ const Navbar = () => {
           </a>
         </div>
       </nav>
+
     </>
   );
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import Parallax from './Parallax.jsx'
+
 import logo from "../assets/logo.png"
 
 function Home() {
   return(
     <>
+      {/*
       <div className="">
         <h1 className="absolute w-full md:hidden text-center font-mono">Blake's Gelato</h1>
         <div className="flex items-center justify-center min-h-screen md:min-h-full">
@@ -14,6 +17,21 @@ function Home() {
       <div className="m-8">
         <p>Blake’s Gelato is a locally made, authentic treat crafted by Blake himself—every batch is handmade with care, ensuring the highest quality and flavor. Based in Maitland, Australia, Blake’s Gelato brings a rich, traditional taste to the community, made with passion and dedication. You can find it at The Woodville Store, Vacy General Store, and Bella and Co. Coffee Cart. Whether you're after a classic favorite or something new, Blake’s Gelato is the perfect way to indulge in a truly artisanal experience.</p>
       </div>
+      <br />
+      <br />
+      */}
+      <Parallax />
+      <div className="">
+        <h1 className="absolute w-full md:hidden text-center font-mono">Blake's Gelato</h1>
+        <div className="flex items-center justify-center min-h-screen md:min-h-full">
+          <img src={logo} className="md:hidden" />
+        </div>
+      </div>
+      <div className="m-8">
+        <p>Blake’s Gelato is a locally made, authentic treat crafted by Blake himself—every batch is handmade with care, ensuring the highest quality and flavor. Based in Maitland, Australia, Blake’s Gelato brings a rich, traditional taste to the community, made with passion and dedication. You can find it at The Woodville Store, Vacy General Store, and Bella and Co. Coffee Cart. Whether you're after a classic favorite or something new, Blake’s Gelato is the perfect way to indulge in a truly artisanal experience.</p>
+      </div>
+      <br />
+      <br />
     </>
   );
 }
